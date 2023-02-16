1c6d554d97b25fa0c437560b9ed626865f0c67cb
Osunronbi Okikiola <osunronbiolanlesi@gmail.com>
2023-02-16 14:53:14 +0100

    setup ratelimiting and cors

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,25 @@ import AppError from "./utils/AppError";
 import globalErrorHandler from "./controllers/errorController";
 import userRouter from "./routes/userRoutes";
 import noteRouter from "./routes/noteRoutes";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(morgan("dev"));
 
+const limiter = {
+  windowMs: 60 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again in an hour",
+};
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://uptick-notes.netlify.app"],
+    credentials: true,
+  })
+);
+app.use(rateLimit(limiter));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
